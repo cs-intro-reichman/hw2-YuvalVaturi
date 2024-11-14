b23a4c6742b816af6b7842b37ba2e878d1ab6f42
YuvalVaturi <yuvalvaturi1@gmail.com>
2024-11-14 17:05:50 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -20,7 +20,9 @@ public class Cheers {
 
         }
         System.out.println((String)"What does that spell?");
-        System.out.println(word + "!!!");
+        if (times == 0){
+                System.out.println(word + "!!!");
+        }
         for (int i = 0; i < times; i ++){
                 System.out.println(word + "!!!");
         }
