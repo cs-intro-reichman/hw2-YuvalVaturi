0cb7605a2a13daf747b8ca0212d82b01c8a67c10
YuvalVaturi <yuvalvaturi1@gmail.com>
2024-11-14 14:03:22 +0200

    Update TimeCalc.java

--- TimeCalc.java
+++ TimeCalc.java
@@ -5,39 +5,46 @@ public class TimeCalc {
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         int minutesToAdd = Integer.parseInt(args[1]);
 
-        System.out.println(hours);
+        int TotalHours = minutesToAdd / 60; // total hours out of minutes
+        int NewHours = TotalHours % 24; // actual hours to move
+        int NewMinutes = (minutesToAdd - (TotalHours * 60)); // actual minutes to add
 
-        // int TotalHours = minutesToAdd / 60; // total hours out of minutes
-        // int NewHours = TotalHours % 24; // actual hours to move
-        // int NewMinutes = (minutesToAdd - (TotalHours * 60)); // actual minutes to add
+        hours = hours + NewHours;
+        minutes = minutes + NewMinutes;
 
-        // hours = hours + NewHours;
-        // minutes = minutes + NewMinutes;
+        if (minutes > 60) {
+            minutes = minutes % 60;
+            hours = hours + 1;
+        }
 
-        // if (minutes > 60) {
-        //     minutes = minutes % 60;
-        //     hours =+ 1;
-        // }
+        if (hours == 24){
+            hours = 00;
+        }
 
-        // if (hours == 24){
-        //     hours = 00;
-        // }
+        else if (hours > 24) {
+            hours = hours % 24;
+        } 
 
-        // else if (hours > 24) {
-        //     hours = hours % 24;
-        // } 
-        
+        String fhours;
+		if (hours < 10) {
+			fhours = "0" + hours;
+		}
+		else {
+			fhours = String.valueOf(minutes);
+		}
     
-        // String fminutes;
-		// if (minutes < 10) {
-		// 	fminutes = "0" + minutes;
-		// }
-		// else {
-		// 	fminutes = String.valueOf(minutes);
-		// }
-        // System.out.println(hours + "," + NewHours + "," + TotalHours);
-        // System.out.println(hours + ":" + fminutes);
+        String fminutes;
+		if (minutes < 10) {
+			fminutes = "0" + minutes;
+		}
+		else {
+			fminutes = String.valueOf(minutes);
+		}
+        
+        System.out.println(fhours + ":" + fminutes);
+        
 // java TimeCalc.java 22:07 12345
 // java TimeCalc.java 23:59 10
+// java TimeCalc.java 10:30 4055
     }
 }
